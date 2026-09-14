ec3e031873b17b23908d0d2cfb8202fb8691e5f9
Rexmar2 <hazerulincuna@gmail.com>
2026-09-15 07:58:39 +0800

    Added database/migration/004_return.sql

---
+++ databse/migration/004_return.sql
@@ -0,0 +1,27 @@
+SELECT 
+    br.borrow_id,
+    s.student_id,
+    CONCAT(s.student_first_name, ' ', 
+    s.student_last_name
+    ) AS student_name,
+    b.book_title,
+    b.book_author,
+    b.book_category,
+    br.borrow_date
+FROM borrow br.
+
+INNER borrow for
+
+
+INNER JOIN students s
+  ON br.student_id = s.student_id
+
+INNER JOIN books b
+  ON br.book_id = b.book_id
+
+WHERE br.borrow_return_date IS NULL
+ORDER BY br.borrow_date DESC;
+UPDATE borrow 
+SET borrow_return_date = CURRENT_TIMESTAMP
+WHERE borrow_id =4
+AND borrow_return_date IS NULL;
